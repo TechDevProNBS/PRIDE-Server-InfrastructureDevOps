ab772f677fe5e947b16bd4206846096cf4786cc1
dumitru-sarov <dumitru7sarov@gmail.com>
2019-12-09 16:56:51 +0000

    changed admin to admin_user in management VM

--- modules/management/virtual-machine.tf
+++ modules/management/virtual-machine.tf
@@ -2,29 +2,26 @@ resource "azurerm_virtual_machine" "manager" {
     name = "${var.prefix}-manager-vm"
     resource_group_name = var.resource_group.name
     location = var.resource_group.location
-    network_interface_ids = [azurerm_network_interface.front_end.id]
+    network_interface_ids = [azurerm_network_interface.manager_nic.id]
     vm_size = "Standard_DS1_v2"
 
-    storage_image_reference {
-    publisher = "credativ"
-    offer     = "Debian"
-    sku       = "9"
+   storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
-    
-    }
+  }
 
     storage_os_disk {
     name              = "${var.prefix}-manager-vm"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-    
+    managed_disk_type = "Standard_LRS"    
     }
 
     os_profile {
     computer_name  = "${var.prefix}-manager-vm"
-    admin_username = var.admin
-    
+    admin_username = var.admin_user
     }
 
     # Remote connection
