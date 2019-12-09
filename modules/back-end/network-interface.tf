f19df1c179c0a6dc17e0da5696ab80bb446141d0
Ephre <jamesdefty@gmail.com>
2019-12-09 16:12:53 +0000

    Updated back end config

---
+++ modules/back-end/network-interface.tf
@@ -0,0 +1,14 @@
+resource "azurerm_network_interface" "back_end" {
+  name                = "${terraform.workspace}-back_end-nic"
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+  network_security_group_id = azurerm_network_security_group.back_end.id
+
+
+  ip_configuration {
+    name                          = "${terraform.workspace}-back_end"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.back_end.id
+  }
+}
---
+++ modules/back-end/virtual-machine.tf
@@ -0,0 +1,32 @@
+resource "azurerm_virtual_machine" "back_end" {
+  name                  = "${terraform.workspace}-back_end-vm"
+  location              = var.resource_group.location
+  resource_group_name   = var.resource_group.name
+  network_interface_ids = [azurerm_network_interface.back_end.id]
+  vm_size               = "Standard_B1ms"
+
+  # May need to bump to "Standard_B2" if 1 core does not suffice
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${terraform.workspace}-back_end-vm"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${terraform.workspace}-back_end-vm"
+    admin_username = var.admin_user
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+	  path = "/home/${var.admin_user}/.ssh/authorized_keys"
+	  key_data = file(pathexpand("~/.ssh/id_rsa.pub"))
+	}
+  }
